5e446db9383bfd155552cc75f9a999120a903bdf
Michał Niebrzydowski <82823@student.pb.edu.pl>
2023-11-21 23:06:25 +0100

    entities

--- src/main/java/pl/edu/pb/wi/mmm/repository/ProductImagesRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.edu.pb.wi.mmm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-//import pl.edu.pb.wi.mmm.entity.ProductImage;
-/*
-public interface ProductImagesRepository extends JpaRepository<ProductImage, Long> {
-}
-*/
