866f6a963fee393248057a1827632ceae4dac156
PBYAB <michalzajkowski71@gmail.com>
2023-12-16 20:34:03 +0100

    removed logging

--- src/main/java/pl/edu/pb/wi/mmm/repository/RequestLogRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.edu.pb.wi.mmm.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pl.edu.pb.wi.mmm.entity.RequestLog;
-
-public interface RequestLogRepository extends JpaRepository<RequestLog, Long> {
-}
